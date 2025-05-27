468cf0985b923d2997e551f3a91c6976f6bf5bf0
zrotrasukha <zrotrasukha@gmail.com>
2025-05-27 22:17:12 +0530

    feat: add sendMail utility for sending emails with dynamic sender and recipient

---
+++ backend/src/utils/sendMail.ts
@@ -0,0 +1,25 @@
+import resend from "../config/resend";
+import { EMAIL_SENDER, NODE_ENV } from "../constants/env";
+
+type emailParams = {
+  to: string,
+  text: string,
+  subject: string,
+  html: string,
+}
+
+const getFromEmail = () =>
+  NODE_ENV === "development" ? "onboarding@resend.dev" : EMAIL_SENDER;
+
+const getToEmail = (to: string) =>
+  NODE_ENV === "development" ? "deliveredTo@resend.dev" : to;
+export const sendMail = async ({ to, text, subject, html }: emailParams) => {
+  const emailData = await resend.emails.send({
+    from: getFromEmail(),
+    to: getToEmail(to),
+    subject,
+    text,
+    html
+  })
+  return { emailData };
+}
