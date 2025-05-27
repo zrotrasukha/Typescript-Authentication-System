665ac579a1ce722729841d4ff01ad1ddb8ec7fa0
zrotrasukha <zrotrasukha@gmail.com>
2025-05-27 22:13:03 +0530

    feat: add omitPassword method to UserDocument interface and implement it in userSchema

--- backend/src/model/user.model.ts
+++ backend/src/model/user.model.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { hashePassword } from '../utils/bcrypt';
 
+
+//NOTE: if you creating a new method under a schema, you need to add it to the interface as well, otherwise, typescript will not recognize it and will throw an error.
 export interface UserDocument extends mongoose.Document {
   email: string;
   password: string;
@@ -8,6 +10,8 @@ export interface UserDocument extends mongoose.Document {
   createdAt: Date;
   updatedAt: Date;
   comparePassword: (password: string) => Promise<boolean>;
+  omitPassword: () => Pick<UserDocument, 'email' | 'verified' | 'createdAt' | 'updatedAt' | 'omitPassword'>;
+  // NOTE: Learned aboud "PICK"; 
 }
 
 export const userSchema = new mongoose.Schema<UserDocument>(
@@ -31,6 +35,14 @@ userSchema.methods.comparePassword = async function(password: string) {
   return await hashePassword(password, this.passwword);
 }
 
+userSchema.methods.omitPassword = function() {
+  const object = this.toObject();
+  delete object.password;
+  return object;
+
+  // NOTE: Learned about toObject method, this is from mongoose and converts the document to a plain JavaScript object, also, it strips all the methods and virtuals from the document, leaving only the fields defined in the schema.
+  // NOTE: Learned about delete 
+}
 const UserModel = mongoose.model<UserDocument>("User", userSchema);
 
 export default UserModel;
