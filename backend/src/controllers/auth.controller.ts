67084764de443dc024e3b29ccfc4c6c5f5899728
zrotrasukha <zrotrasukha@gmail.com>
2025-05-27 22:12:38 +0530

    feat: refactor authentication handlers for improved error handling and session management

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,33 +1,89 @@
 import catchErrors from "../utils/catchError";
-import { z } from "zod/v4";
-import { createUser } from "../services/auth.services";
-import { BAD_REQUEST, CREATED } from "../constants/statusCodes";
-import { setAuthCookies } from "../utils/cookies";
-
-const registerSchema = z.object({
-  email: z.email().min(3).max(255),
-  password: z.string().min(8).max(255),
-  confirmPassword: z.string().min(8).max(255),
-  userAgent: z.string().optional(),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "Passwords do not match",
-  path: ["confirmPassword"],
-})
-
-// to handle refine part we gotta use zod error, for that go back to catchError.ts
-//
+import { createUser, loginUser, refreshUserAccessToken, verifyemail } from "../services/auth.services";
+import { BAD_REQUEST, CREATED, OK, UNAUTHORIZED } from "../constants/statusCodes";
+import { deleteAuthCookies, getAccessTokenCookieOptions, getRefreshTokenCookieOptions, setAuthCookies } from "../utils/cookies";
+import { registerSchema } from "./auth.schema";
+import { loginSchema } from "./auth.schema";
+import appAssert from "../utils/AppAssert";
+import { refreshTokenPayload, verifyToken } from "../utils/jwt";
+import sessionModel from "../model/session.model";
+import { verificationSchema } from "./auth.schema";
 export const registerHandler = catchErrors(async (req, res) => {
+  // NOTE: zodSchema.parse() creates and returns a deeply typed copy of the input data, validating it against the schema. If the data is valid, it returns the parsed data; if not, it throws an error.
   const request = registerSchema.parse({
     ...req.body,
     userAgent: req.headers["user-agent"],
+    //NOTE: userAgent was not added in registerSchema because it comes from headers and not from body
+    //NOTE: This userAgent is being used while creating a session in the auth.services.ts so that we can find out what device the user is using to log in. 
   })
   if (!request) {
-    return res.status(BAD_REQUEST).json({
-      message: "Invalid request",
-    })
+    appAssert(request, BAD_REQUEST, "Invalid request data");
+    return;
   }
 
   const { user, accessToken, refreshToken } = await createUser(request);
 
-  return setAuthCookies({ res, accessToken, refreshToken }).status(CREATED).json({ user });
-}); 
+  return setAuthCookies({
+    res,
+    accessToken,
+    refreshToken
+  }).status(CREATED).json({ user });
+});
+
+export const loginHandler = catchErrors(async (req, res) => {
+  const request = loginSchema.parse({
+    ...req.body,
+    userAgent: req.headers["user-agent"]
+  })
+  appAssert(
+    request,
+    UNAUTHORIZED,
+    "WRONG CREDENTIALS"
+  )
+
+  const { accessToken, refreshToken } = await loginUser(request);
+  return setAuthCookies({
+    res,
+    accessToken,
+    refreshToken
+  }).status(OK).json({ message: "Login successful" });
+
+})
+
+export const logoutHandler = catchErrors(async (req, res) => {
+  const accessToken = req.cookies.accessToken as string | undefined;
+  const { payload } = verifyToken(accessToken || "");
+
+  if (payload) {
+    await sessionModel.findByIdAndDelete(payload.sessionId);
+  }
+  return deleteAuthCookies(res).status(OK).json({ message: "Logout successful" });
+})
+
+export const refreshTokenHandler = catchErrors(async (req, res) => {
+  const prevRefreshToken = req.cookies.refreshToken as string | undefined;
+
+  appAssert(
+    prevRefreshToken,
+    UNAUTHORIZED,
+    "Refresh token is required"
+  );
+
+  const { accessToken, refreshToken } = await refreshUserAccessToken(prevRefreshToken);
+
+  if (refreshToken) {
+    res.cookie("refreshToken", refreshToken, getRefreshTokenCookieOptions());
+  }
+
+  return res
+    .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
+    .status(OK)
+    .json({ message: "Access token refreshed successfully" });
+
+})
+
+export const verifyEmailHandler = catchErrors(async (req, res) => {
+  const verificationCode = verificationSchema.parse(req.params.code); 
+  await verifyemail(verificationCode);
+  return res.status(OK).json({ message: "Email verified successfully" });
+})
