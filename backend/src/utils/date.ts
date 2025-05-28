46314daaf4ab3121bcdab82f69ab7d80817394b2
zrotrasukha <zrotrasukha@gmail.com>
2025-05-29 02:18:54 +0530

    fix: correct function names and improve date utility exports

--- backend/src/utils/date.ts
+++ backend/src/utils/date.ts
@@ -6,7 +6,10 @@ export const oneYearFromNow = () =>
 
 export const fifteenminutesFromNow = () =>
   new Date(Date.now() + 15 * 60 * 1000);
-export const tenmintuseFromNow = () => 
+export const tenmintuseFromNow = () =>
   new Date(Date.now() + 10 * 60 * 1000);
 
 export const msInADay = 24 * 60 * 60 * 1000;
+
+export const fiveMinuteAgo = () => new Date(Date.now() - (1000 * 60 * 5));
+export const fiveMinutesFromNow = () => new Date(Date.now() + 1000 * 60 * 5); 
