365760e45db76b931c89dba4e03d0279c28b0a71
zrotrasukha <zrotrasukha@gmail.com>
2025-05-29 02:17:30 +0530

    feat: add password reset and email handling routes to authentication router

--- backend/src/router/auth.router.ts
+++ backend/src/router/auth.router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginHandler, logoutHandler, refreshTokenHandler, registerHandler, verifyEmailHandler } from "../controllers/auth.controller";
+import { loginHandler, logoutHandler, refreshTokenHandler, registerHandler, resetPasswordHandler, sendPasswordResetEmailHandler, verifyEmailHandler } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
@@ -8,5 +8,6 @@ authRouter.post("/login", loginHandler);
 authRouter.get('/logout', logoutHandler);
 authRouter.get('/refresh', refreshTokenHandler);
 authRouter.get('/email/verify/:code', verifyEmailHandler);
-
+authRouter.post('/password/forgot', sendPasswordResetEmailHandler);
+authRouter.post('/password/reset', resetPasswordHandler);
 export default authRouter; 
