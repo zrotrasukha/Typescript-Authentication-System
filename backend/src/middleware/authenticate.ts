2479ae62ee4256f094a40e9cdf6b9243ed49ba9a
zrotrasukha <zrotrasukha@gmail.com>
2025-05-29 02:17:14 +0530

    feat: add authentication middleware with token verification and error handling

---
+++ backend/src/middleware/authenticate.ts
@@ -0,0 +1,29 @@
+import AppErrorCode from "../constants/appErrorCode";
+import { UNAUTHORIZED } from "../constants/statusCodes";
+import { payloadType } from "../controllers/auth.schema";
+import appAssert from "../utils/AppAssert";
+import catchErrors from "../utils/catchError";
+import { verifyToken } from "../utils/jwt";
+
+const authenticate = catchErrors(async (req, res, next) => {
+  const accessToken = req.cookies.accessToken as string | undefined;
+  appAssert(
+    accessToken,
+    UNAUTHORIZED,
+    "Not authorized",
+    AppErrorCode.InvalidAccessToken
+  )
+  const { error, payload } = verifyToken<payloadType>(accessToken);
+  appAssert(
+    payload,
+    UNAUTHORIZED,
+    error === "jwt expired" ? "Token expired" : "Invalid access token",
+    AppErrorCode.InvalidAccessToken
+  )
+
+  req.userId = payload?.userId;
+  req.sessionId = payload?.sessionId;
+  next();
+})
+
+export default authenticate
